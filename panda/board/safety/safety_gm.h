d04fafe69a8f11477dc5e3d51952e34a9a334f82
parksunkyu81 <parksunkyu81@gmail.com>
2022-03-17 20:19:07 +0900

    이쿼녹스 추가

--- panda/board/safety/safety_gm.h
+++ panda/board/safety/safety_gm.h
@@ -117,9 +117,9 @@ static int gm_rx_hook(CANPacket_t *to_push) {
         case 3:  // set
           controls_allowed = 1;
           break;
-        case 6:  // cancel
-          controls_allowed = 0;
-          break;
+        //case 6:  // cancel
+        //  controls_allowed = 0;
+        //  break;
         default:
           break;  // any other button is irrelevant
       }
