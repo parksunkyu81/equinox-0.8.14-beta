fbee83565771273839f73f7d48ac621ca7661dc6
parksunkyu81 <parksunkyu81@gmail.com>
2022-03-22 09:40:13 +0900

    이쿼녹스 추가

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -853,18 +853,12 @@ EVENTS: Dict[int, Dict[str, Union[Alert, AlertCallbackType]]] = {
   },
 
   EventName.slowingDownSpeed: {
-    ET.PERMANENT: Alert(
-      "Slowing down",
-      "",
-      AlertStatus.normal, AlertSize.small,
+    ET.PERMANENT: Alert("Slowing down","", AlertStatus.normal, AlertSize.small,
       Priority.LOW, VisualAlert.none, AudibleAlert.none, .1),
   },
 
   EventName.slowingDownSpeedSound: {
-    ET.PERMANENT: Alert(
-      "Slowing down",
-      "",
-      AlertStatus.normal, AlertSize.small,
+    ET.PERMANENT: Alert("Slowing down","", AlertStatus.normal, AlertSize.small,
       Priority.LOW, VisualAlert.none, AudibleAlert.slowingDownSpeed, 2.),
   },
 
