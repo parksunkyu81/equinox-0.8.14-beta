d1aeb1627ebaf2eff4db06c941b5b32248cde034
parksunkyu81 <parksunkyu81@strato.co.kr>
2022-03-30 18:20:25 +0900

    이쿼녹스 추가

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -444,7 +444,7 @@ void NvgWindow::drawHud(QPainter &p) {
 
   QColor orangeColor = QColor(0, 191, 255, 255);
 
-  int x = 730;
+  int x = 700;
   int y = rect().height() - 15;
 
   QString infoText;
