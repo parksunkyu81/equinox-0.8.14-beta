614359f1c178f9987cd31f3d99e4079a8ff16b83
parksunkyu81 <parksunkyu81@gmail.com>
2022-04-02 15:04:30 +0900

    이쿼녹스 추가

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -21,7 +21,7 @@ class CarInterface(CarInterfaceBase):
     #gas_max_bp = [0.0, 5.0, 9.0, 35.0]  # felger
     #gas_max_v =  [0.4, 0.5, 0.7, 0.7]
     gas_max_bp = [0., 10. * CV.KPH_TO_MS, 25. * CV.KPH_TO_MS, 40. * CV.KPH_TO_MS, 60. * CV.KPH_TO_MS, 80. * CV.KPH_TO_MS, 100. * CV.KPH_TO_MS, 110. * CV.KPH_TO_MS]
-    gas_max_v = [0.2, 0.25, 0.3, 0.38, 0.4, 0.5, 0.6, 0.6]
+    gas_max_v = [0.2, 0.25, 0.35, 0.4, 0.45, 0.55, 0.65, 0.7]
     brake_max_bp = [0, 70., 130.]
     brake_max_v = [-4., -3., -2.1]
     return interp(v_current_kph, brake_max_bp, brake_max_v), interp(v_current_kph, gas_max_bp, gas_max_v)
@@ -114,7 +114,7 @@ class CarInterface(CarInterfaceBase):
 
     ret.longitudinalTuning.kpBP = [0., 10. * CV.KPH_TO_MS, 25. * CV.KPH_TO_MS, 40. * CV.KPH_TO_MS, 60. * CV.KPH_TO_MS,
                                    80. * CV.KPH_TO_MS, 100. * CV.KPH_TO_MS, 110. * CV.KPH_TO_MS]
-    ret.longitudinalTuning.kpV = [1.15, 1.02, 0.8, 0.75, 0.72, 0.65, 0.6, 0.52]
+    ret.longitudinalTuning.kpV = [1.15, 1.02, 0.8, 0.75, 0.72, 0.73, 0.75, 0.75]
     ret.longitudinalTuning.kiBP = [0., 5., 12., 20., 27.]
     ret.longitudinalTuning.kiV = [.35, .23, .20, .17, .1]
     ret.longitudinalTuning.deadzoneBP = [0., 8.05]
