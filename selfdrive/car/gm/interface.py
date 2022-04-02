de1b176595234198b04d6cb95e14b58986de9a6b
parksunkyu81 <parksunkyu81@gmail.com>
2022-04-02 11:23:15 +0900

    이쿼녹스 추가

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -18,8 +18,10 @@ class CarInterface(CarInterfaceBase):
     #params = CarControllerParams(CP)
     #return params.ACCEL_MIN, params.ACCEL_MAX
     v_current_kph = current_speed * CV.MS_TO_KPH
-    gas_max_bp = [0.0, 5.0, 9.0, 35.0]  # felger
-    gas_max_v = [0.3, 0.4, 0.5, 0.5]
+    #gas_max_bp = [0.0, 5.0, 9.0, 35.0]  # felger
+    #gas_max_v =  [0.4, 0.5, 0.7, 0.7]
+    gas_max_bp = [0., 10. * CV.KPH_TO_MS, 25. * CV.KPH_TO_MS, 40. * CV.KPH_TO_MS, 60. * CV.KPH_TO_MS, 80. * CV.KPH_TO_MS, 100. * CV.KPH_TO_MS, 110. * CV.KPH_TO_MS]
+    gas_max_v = [0.2, 0.25, 0.27, 0.32, 0.35, 0.4, 0.5, 0.6]
     brake_max_bp = [0, 70., 130.]
     brake_max_v = [-4., -3., -2.1]
     return interp(v_current_kph, brake_max_bp, brake_max_v), interp(v_current_kph, gas_max_bp, gas_max_v)
