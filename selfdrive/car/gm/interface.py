6e185e96fb3e41931396765eaaa0918da2a2a181
parksunkyu81 <parksunkyu81@gmail.com>
2022-04-02 23:25:38 +0900

    이쿼녹스 추가

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -115,7 +115,7 @@ class CarInterface(CarInterfaceBase):
 
     # longitudinal
     ret.longitudinalTuning.kpBP = [0., 25. * CV.KPH_TO_MS, 40. * CV.KPH_TO_MS, 80. * CV.KPH_TO_MS, 100. * CV.KPH_TO_MS]
-    ret.longitudinalTuning.kpV = [1.2, 1.1, 0.85, 0.73, 0.65]
+    ret.longitudinalTuning.kpV = [1.1, 1.0, 0.8, 0.73, 0.65]
 
     ret.longitudinalTuning.kiBP = [0., 130. * CV.KPH_TO_MS]
     ret.longitudinalTuning.kiV = [0.18, 0.12]
