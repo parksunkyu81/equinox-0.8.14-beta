5337d8d5f003f57ccad0ef11ec2eaef82a77d57c
parksunkyu81 <parksunkyu81@gmail.com>
2022-04-16 11:31:25 +0900

    이쿼녹스 추가

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -108,25 +108,19 @@ class CarInterface(CarInterfaceBase):
     #ret.longitudinalTuning.kiBP = [0., 100. * CV.KPH_TO_MS]
     #ret.longitudinalTuning.kiV = [0.18, 0.12]
 
-    # START OPKR
-    ret.longitudinalTuning.kpBP = [0., 4., 9., 17., 23., 31.]
-    ret.longitudinalTuning.kpV = [1.2, 1.1, 1.0, 0.9, 0.75, 0.65]
-    ret.longitudinalTuning.kiBP = [0., 4., 9., 17., 23., 31.]
-    ret.longitudinalTuning.kiV = [0.27, 0.24, 0.23, 0.2, 0.17, 0.15]
-    ret.longitudinalTuning.kdBP = [0., 4., 9., 17., 23., 31.]
-    ret.longitudinalTuning.kdV = [0.9, 1.0, 0.85, 0.7, 0.5, 0.4]
+    # longitudinal
+    ret.longitudinalTuning.kpBP = [0., 5. * CV.KPH_TO_MS, 10. * CV.KPH_TO_MS, 20. * CV.KPH_TO_MS, 130. * CV.KPH_TO_MS]
+    ret.longitudinalTuning.kpV = [1.25, 1.1, 1.0, 0.85, 0.48]
+    ret.longitudinalTuning.kiBP = [0., 130. * CV.KPH_TO_MS]
+    ret.longitudinalTuning.kiV = [0.08, 0.03]
+    ret.longitudinalActuatorDelayLowerBound = 0.5
+    ret.longitudinalActuatorDelayUpperBound = 0.5
 
-    ret.longitudinalTuning.deadzoneBP = [0., 4.]
-    ret.longitudinalTuning.deadzoneV = [0., 0.1]
+    ret.stopAccel = -2.0
+    ret.stoppingDecelRate = 0.5  # brake_travel/s while trying to stop
+    ret.vEgoStopping = 0.5
+    ret.vEgoStarting = 0.5
 
-    ret.stoppingControl = False
-    ret.vEgoStopping = 0.5  # 1.0, 0.5
-    ret.vEgoStarting = 0.5  # needs to be >= vEgoStopping to avoid state transition oscillation
-    ret.stopAccel = -0.5 # 0.0, -0.5
-    ret.stoppingDecelRate = 3.0 # 0.8, 0.2  # brake_travel/s while trying to stop
-    ret.longitudinalActuatorDelayLowerBound = 1.0
-    ret.longitudinalActuatorDelayUpperBound = 1.0
-    # END OPKR
 
     ret.steerLimitTimer = 0.4
     ret.radarTimeStep = 0.0667  # GM radar runs at 15Hz instead of standard 20Hz
