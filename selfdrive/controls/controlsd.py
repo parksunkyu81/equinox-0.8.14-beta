82987ad6ea21bfa4a3298a41e903d46b4c6ca29d
parksunkyu81 <parksunkyu81@gmail.com>
2022-04-20 19:27:03 +0900

    수정

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -893,7 +893,7 @@ class Controls:
             controlsState.vCruise = float(controlsState.applyMaxSpeed)
 
         controlsState.upAccelCmd = float(self.LoC.pid.p)
-        controlsState.uiAccelCmd = float(self.LoC.pid.id)
+        controlsState.uiAccelCmd = float(self.LoC.pid.i)
         controlsState.ufAccelCmd = float(self.LoC.pid.f)
         controlsState.cumLagMs = -self.rk.remaining * 1000.
         controlsState.startMonoTime = int(start_time * 1e9)
