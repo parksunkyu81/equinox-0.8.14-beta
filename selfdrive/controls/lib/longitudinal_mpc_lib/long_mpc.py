ce4a9abd21b5a51a6e5e67a3fdc2f3d7ea9c2358
parksunkyu81 <parksunkyu81@gmail.com>
2022-04-29 09:41:34 +0900

    앞차 거리 튜닝

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -55,7 +55,7 @@ ACADOS_SOLVER_TYPE = 'SQP_RTI'
 # 초반은 민첩하게, 중후반은 멀리보게 (퓨얼컷)
 AUTO_TR_BP = [0., 5.*CV.KPH_TO_MS, 10.*CV.KPH_TO_MS, 20.*CV.KPH_TO_MS,
               30.*CV.KPH_TO_MS, 35.*CV.KPH_TO_MS, 50.*CV.KPH_TO_MS, 70.*CV.KPH_TO_MS, 110*CV.KPH_TO_MS]
-AUTO_TR_V = [0.0, 0.3, 0.5, 0.9, 2.2, 2.5, 3.0, 3.5, 3.8]
+AUTO_TR_V = [0.0, 0.3, 0.5, 0.9, 1.8, 2.5, 3.0, 3.5, 3.8]
 
 # 1 Seconds = 12 Meter
 # 1 Meter = 0.084
