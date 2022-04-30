9fdacd456273311b830230cb08f74462e76074e0
parksunkyu81 <parksunkyu81@gmail.com>
2022-04-30 19:58:07 +0900

    앞차 거리 튜닝

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -17,15 +17,8 @@ class CarInterface(CarInterfaceBase):
 
   @staticmethod
   def get_pid_accel_limits(CP, current_speed, cruise_speed):
-    #params = CarControllerParams(CP)
-    #return params.ACCEL_MIN, params.ACCEL_MAX
-    v_current_kph = current_speed * CV.MS_TO_KPH
-
-    gas_max_bp = [10., 20., 50., 70., 130., 150.]
-    gas_max_v = [1.5, 1.25, 0.67, 0.47, 0.16, 0.1]
-
-    return CarControllerParams.ACCEL_MIN, interp(v_current_kph, gas_max_bp, gas_max_v)
-
+    params = CarControllerParams(CP)
+    return params.ACCEL_MIN, params.ACCEL_MAX
 
 
   # Determined by iteratively plotting and minimizing error for f(angle, speed) = steer.
