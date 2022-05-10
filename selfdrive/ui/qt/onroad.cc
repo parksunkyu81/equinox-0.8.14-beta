7b35eb27a2b8f30ebae28555120efb764ccb9292
parksunkyu81 <parksunkyu81@gmail.com>
2022-05-10 19:36:31 +0900

    fix

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -736,8 +736,8 @@ void NvgWindow::drawBottomIcons(QPainter &p) {
   // 5. regen
   x = radius / 2 + (bdr_s * 2) + ((radius + 50) * 4);
   bool regen_valid = car_control.getActuators().getRegenPaddle();
-  img_alpha = brake_valid ? 1.0f : 0.15f;
-  bg_alpha = brake_valid ? 0.3f : 0.1f;
+  img_alpha = regen_valid ? 1.0f : 0.15f;
+  bg_alpha = regen_valid ? 0.3f : 0.1f;
   drawIcon(p, x, y2, ic_regenPaddle, QColor(0, 0, 0, (255 * bg_alpha)), img_alpha);
   p.setOpacity(1.0);
 
