da2d2704ec75b2c7decbf0496c1482b8fb19197d
parksunkyu81 <parksunkyu81@strato.co.kr>
2022-05-20 10:08:33 +0900

    Tuning

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -21,7 +21,7 @@ class CarInterface(CarInterfaceBase):
     v_current_kph = current_speed * CV.MS_TO_KPH
     # return params.ACCEL_MIN, params.ACCEL_MAX
     accel_max_bp = [10., 20., 50.]
-    accel_max_v = [1.47, 1.435, 1.35]
+    accel_max_v = [1.46, 1.43, 1.35]
 
     return params.ACCEL_MIN, interp(v_current_kph, accel_max_bp, accel_max_v)
 
@@ -100,10 +100,10 @@ class CarInterface(CarInterfaceBase):
     else:
       ret.lateralTuning.init('torque')
       ret.lateralTuning.torque.useSteeringAngle = True
-      max_lat_accel = 2.15  # 코너가 부족하면 max_lat_accel을 Down
+      max_lat_accel = 2.275  # 코너가 부족하면 max_lat_accel을 Down (DEF: 2.15)
       ret.lateralTuning.torque.kp = 2.0 / max_lat_accel
       ret.lateralTuning.torque.kf = 1.0 / max_lat_accel
-      ret.lateralTuning.torque.ki = 0.2 / max_lat_accel
+      ret.lateralTuning.torque.ki = 0.19 / max_lat_accel
       ret.lateralTuning.torque.friction = 0.02
 
       ret.lateralTuning.torque.kd = 1.0
@@ -113,7 +113,7 @@ class CarInterface(CarInterfaceBase):
     # steerActuatorDelay, steerMaxV 커질수록 인으로 붙고, scale 작을수록 인으로 붙는다.
     # steerratecost를 높이면 핸들링이 부드러워(둔감)해 집니다. 다시 말해 도로의 작은 변화에 기민하게 반응하지 않게 됩니다.
     # steeractuatordelay는 계산된 주행곡선을 좀더 빠르게 혹은 느리게 반영할지를 결정합니다
-    ret.steerActuatorDelay = 0.25    # DEF : 0.1  너무 늦게 선회하면 steerActuatorDelay를 늘립니다.
+    ret.steerActuatorDelay = 0.24    # DEF : 0.1  너무 늦게 선회하면 steerActuatorDelay를 늘립니다.
     ret.steerRateCost = 0.35        # DEF : 0.4  조향 속도에 대한 측면 MPC 비용
     ret.steerLimitTimer = 0.4       # steerLimitAlert 가 발행되기 전의 시간 (핸들 조향을 하는데 100을 하라고 명령을 했는데, 그걸 해내는데 리미트 시간)
 
