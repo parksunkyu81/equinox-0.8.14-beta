5efc8ec4b76d928efae0d931d9b97c3403becba2
parksunkyu81 <parksunkyu81@gmail.com>
2022-05-23 09:08:30 +0900

    Tuning

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -275,7 +275,7 @@ class Controls:
             if lead is not None:
                 # d : 비전 거리
                 d = lead.dRel
-                if 0. < d < -lead.vRel * 30.:
+                if 0. < d < -lead.vRel * 45.:
                     t = d / lead.vRel
                     accel = -(lead.vRel / t) * self.speed_conv_to_clu
                     accel *= 1.2
@@ -285,7 +285,7 @@ class Controls:
                         target_speed = max(target_speed, self.kph_to_clu(30))
                         return target_speed
 
-                elif 0. < d < -lead.vRel * 40.:
+                """elif 0. < d < -lead.vRel * 40.:
                     t = d / lead.vRel
                     accel = -(lead.vRel / t) * self.speed_conv_to_clu
                     accel *= 1.2
@@ -295,7 +295,7 @@ class Controls:
                         target_speed = max(target_speed, self.kph_to_clu(40))
                         return target_speed
 
-                """elif 0. < d < -lead.vRel * 55.:
+                elif 0. < d < -lead.vRel * 55.:
                     t = d / lead.vRel
                     accel = -(lead.vRel / t) * self.speed_conv_to_clu
                     accel *= 1.2
