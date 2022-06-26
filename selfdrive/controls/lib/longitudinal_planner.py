bf13826ccf7c1da99c465b5eabda629723187e2a
parksunkyu81 <parksunkyu81@gmail.com>
2022-06-26 17:13:29 +0900

    equinox tuning

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -25,11 +25,11 @@ A_CRUISE_MAX_BP = [0., 15., 25., 40.]
 _A_TOTAL_MAX_V = [1.7, 3.2]
 _A_TOTAL_MAX_BP = [20., 40.]
 
-_DP_CRUISE_MIN_V = [-3.5, -3.3, -3.0, -2.8, -2.3]
-_DP_CRUISE_MIN_V_FOLLOWING = [-4.5, -4.3, -4.0, -3.5, -3.0]
+_DP_CRUISE_MIN_V = [-5.5, -4.3, -4.0, -3.5, -3.0]
+_DP_CRUISE_MIN_V_FOLLOWING = [-3.5, -3.0, -2.8, -2.5, -2.0]
 _DP_CRUISE_MIN_BP = [0.0, 5.0, 10.0, 20.0, 30.0]
 
-_DP_CRUISE_MAX_V = [2.0, 1.8, 1.5, 1.3, 0.4]
+_DP_CRUISE_MAX_V = [1.3, 1.2, 0.8, 0.65, 0.5]
 _DP_CRUISE_MAX_V_FOLLOWING = [2.0, 1.8, 1.5, 1.3, 0.4]
 _DP_CRUISE_MAX_BP = [0., 5., 10., 20., 30.]
 
@@ -99,7 +99,7 @@ class Planner:
 
     # following dist
     lead_1 = sm['radarState'].leadOne
-    following = lead_1.status and lead_1.dRel < 30.0 and lead_1.vLeadK > v_ego and lead_1.aLeadK > 0.0
+    following = lead_1.status and lead_1.dRel < 40.0 and lead_1.vLeadK > v_ego and lead_1.aLeadK > 0.0
 
     # Prevent divergence, smooth in current v_ego
     self.v_desired_filter.x = max(0.0, self.v_desired_filter.update(v_ego))
