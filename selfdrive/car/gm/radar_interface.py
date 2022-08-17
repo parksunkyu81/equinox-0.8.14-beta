85ad84345a877361a35d03fc7a996983a7362ffd
parksunkyu81 <parksunkyu81@gmail.com>
2022-08-17 18:32:34 +0900

    Revert "Revert "Tuning""
    This reverts commit 266289ce

--- selfdrive/car/gm/radar_interface.py
+++ selfdrive/car/gm/radar_interface.py
@@ -16,23 +16,7 @@ LAST_RADAR_MSG = RADAR_HEADER_MSG + NUM_SLOTS
 
 
 def create_radar_can_parser(car_fingerprint):
-  if car_fingerprint not in (CAR.VOLT, CAR.MALIBU, CAR.HOLDEN_ASTRA, CAR.ACADIA, CAR.CADILLAC_ATS, CAR.ESCALADE_ESV):
     return None
-
-  # C1A-ARS3-A by Continental
-  radar_targets = list(range(SLOT_1_MSG, SLOT_1_MSG + NUM_SLOTS))
-  signals = list(zip(['FLRRNumValidTargets',
-                      'FLRRSnsrBlckd', 'FLRRYawRtPlsblityFlt',
-                      'FLRRHWFltPrsntInt', 'FLRRAntTngFltPrsnt',
-                      'FLRRAlgnFltPrsnt', 'FLRRSnstvFltPrsntInt'] +
-                     ['TrkRange'] * NUM_SLOTS + ['TrkRangeRate'] * NUM_SLOTS +
-                     ['TrkRangeAccel'] * NUM_SLOTS + ['TrkAzimuth'] * NUM_SLOTS +
-                     ['TrkWidth'] * NUM_SLOTS + ['TrkObjectID'] * NUM_SLOTS,
-                     [RADAR_HEADER_MSG] * 7 + radar_targets * 6))
-
-  checks = list({(s[1], 14) for s in signals})
-
-  return CANParser(DBC[car_fingerprint]['radar'], signals, checks, CanBus.OBSTACLE)
 
 class RadarInterface(RadarInterfaceBase):
   def __init__(self, CP):
