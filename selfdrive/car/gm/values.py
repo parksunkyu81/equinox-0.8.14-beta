d2d6e94a9039d4573ad52fc8bd20153565200963
parksunkyu81 <parksunkyu81@gmail.com>
2022-08-19 03:02:40 +0900

    Revert "Tuning"
    This reverts commit 12a9c4f0e4ef3cb315f8339f862ddefbd8e6d983.

--- selfdrive/car/gm/values.py
+++ selfdrive/car/gm/values.py
@@ -12,14 +12,14 @@ MIN_CURVE_SPEED = 40. * CV.KPH_TO_MS  # 커브 속도 설정
 
 class CarControllerParams():
 
-    ACCEL_MAX = 2.  # m/s^2
-    ACCEL_MIN = -4.  # m/s^2
+    ACCEL_MAX = 2.0
+    ACCEL_MIN = -3.5   # 브레이크에 의미가 없다. def : -3.5 (엑셀만 때는 경우 속도)
 
     def __init__(self, CP):
         self.STEER_MAX = 300  # Safety limit, not LKA max. Trucks use 600.
-        self.STEER_STEP = 2  # control frames per command
-        self.STEER_DELTA_UP = 7     # DEF : 8 (낮을수록 헨들을 빠르게 돌린다.)
-        self.STEER_DELTA_DOWN = 17
+        self.STEER_STEP = 4  # control frames per command
+        self.STEER_DELTA_UP = 8     # DEF : 8 (낮을수록 헨들을 빠르게 돌린다.)
+        self.STEER_DELTA_DOWN = 12
         self.MIN_STEER_SPEED = 3.  # m/s
         self.STEER_DRIVER_ALLOWANCE = 50
         self.STEER_DRIVER_MULTIPLIER = 4
