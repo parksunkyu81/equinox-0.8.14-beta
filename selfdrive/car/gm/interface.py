2b5224fe0dbc798a8c0c908ee31c8c56baf99511
parksunkyu81 <parksunkyu81@gmail.com>
2022-08-19 03:18:15 +0900

    Revert "Revert "Revert "Tuning"""
    This reverts commit e552666e99501520dc1fee4b901c29c580ded420.

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -18,8 +18,8 @@ GearShifter = car.CarState.GearShifter
 class CarInterface(CarInterfaceBase):
 
     @staticmethod
-    def get_pid_accel_limits(current_speed, cruise_speed):
-        params = CarControllerParams()
+    def get_pid_accel_limits(CP, current_speed, cruise_speed):
+        params = CarControllerParams(CP)
         return params.ACCEL_MIN, params.ACCEL_MAX
         #accel_max_bp = [10., 20., 30., 50.]
         #accel_max_v = [1.45, 1.425, 1.455, 1.35]
