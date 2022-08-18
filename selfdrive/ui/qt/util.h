dd5fd0cb829fc5a98479f1d3aa14444d75c2d8f1
parksunkyu81 <parksunkyu81@gmail.com>
2022-08-19 03:04:03 +0900

    Revert "Revert "Revert "Tuning"""
    This reverts commit 307294774d58808519dda39363bde98c7f38b953.

--- selfdrive/ui/qt/util.h
+++ selfdrive/ui/qt/util.h
@@ -22,7 +22,3 @@ void swagLogMessageHandler(QtMsgType type, const QMessageLogContext &context, co
 void initApp(int argc, char *argv[]);
 QWidget* topWidget (QWidget* widget);
 QPixmap loadPixmap(const QString &fileName, const QSize &size = {}, Qt::AspectRatioMode aspectRatioMode = Qt::KeepAspectRatio);
-
-QRect getTextRect(QPainter &p, int flags, QString text);
-void drawRoundedRect(QPainter &painter, const QRectF &rect, qreal xRadiusTop, qreal yRadiusTop, qreal xRadiusBottom, qreal yRadiusBottom);
-QColor interpColor(float xv, std::vector<float> xp, std::vector<QColor> fp);
