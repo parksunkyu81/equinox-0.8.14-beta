12a9c4f0e4ef3cb315f8339f862ddefbd8e6d983
parksunkyu81 <parksunkyu81@gmail.com>
2022-08-18 09:09:53 +0900

    Tuning

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -20,12 +20,11 @@ class CarInterface(CarInterfaceBase):
     @staticmethod
     def get_pid_accel_limits(CP, current_speed, cruise_speed):
         params = CarControllerParams(CP)
-        v_current_kph = current_speed * CV.MS_TO_KPH
-        # return params.ACCEL_MIN, params.ACCEL_MAX
-        accel_max_bp = [10., 20., 50.]
-        accel_max_v = [1.45, 1.425, 1.35]
-
-        return params.ACCEL_MIN, interp(v_current_kph, accel_max_bp, accel_max_v)
+        return params.ACCEL_MIN, params.ACCEL_MAX
+        #v_current_kph = current_speed * CV.MS_TO_KPH
+        #accel_max_bp = [10., 20., 50.]
+        #accel_max_v = [1.45, 1.425, 1.35]
+        #return params.ACCEL_MIN, interp(v_current_kph, accel_max_bp, accel_max_v)
 
     # Determined by iteratively plotting and minimizing error for f(angle, speed) = steer.
     @staticmethod
@@ -141,8 +140,8 @@ class CarInterface(CarInterfaceBase):
 
         # longitudinal
         ret.longitudinalTuning.kpBP = [0., 5. * CV.KPH_TO_MS, 10. * CV.KPH_TO_MS, 30. * CV.KPH_TO_MS,
-                                       130. * CV.KPH_TO_MS]
-        ret.longitudinalTuning.kpV = [1.2, 1.0, 0.93, 0.88, 0.5]
+                                       50. * CV.KPH_TO_MS, 80. * CV.KPH_TO_MS, 130. * CV.KPH_TO_MS]
+        ret.longitudinalTuning.kpV = [1.2, 1.0, 0.93, 0.91, 0.88, 0.8, 0.5]
         ret.longitudinalTuning.kiBP = [0., 130. * CV.KPH_TO_MS]
         ret.longitudinalTuning.kiV = [0.1, 0.05]
         ret.longitudinalActuatorDelayLowerBound = 0.3
