183da674e8c89efd7557fc83a51130fb9303fe9c
parksunkyu81 <parksunkyu81@gmail.com>
2022-08-30 08:13:32 +0900

    Tuning

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -126,9 +126,10 @@ class CarInterface(CarInterfaceBase):
         ret.longitudinalActuatorDelayUpperBound = 0.25"""
 
         # longitudinal
+        # 60키로 속도에서 높은 과속
         ret.longitudinalTuning.kpBP = [0., 5. * CV.KPH_TO_MS, 10. * CV.KPH_TO_MS, 30. * CV.KPH_TO_MS,
                                        50. * CV.KPH_TO_MS, 80. * CV.KPH_TO_MS, 130. * CV.KPH_TO_MS]
-        ret.longitudinalTuning.kpV = [1.2, 1.0, 0.93, 0.91, 0.88, 0.8, 0.5]
+        ret.longitudinalTuning.kpV = [1.2, 1.0, 0.93, 0.91, 0.86, 0.78, 0.5]
         ret.longitudinalTuning.kiBP = [0., 130. * CV.KPH_TO_MS]
         ret.longitudinalTuning.kiV = [0.1, 0.05]
         ret.longitudinalActuatorDelayLowerBound = 0.3
