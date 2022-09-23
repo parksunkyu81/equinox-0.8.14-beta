9f8046b7ef63ed92012506cb04dcb4c29b340f8b
parksunkyu81 <parksunkyu81@gmail.com>
2022-09-24 08:03:32 +0900

    Revert "..."
    This reverts commit 3d19bcd0aed2c1cd2cbbdaa3bed245c25e3e21fc.

--- pyextra/acados_template/acados_ocp_solver.py
+++ pyextra/acados_template/acados_ocp_solver.py
@@ -820,44 +820,6 @@ class AcadosOcpSolver:
         dlclose.argtypes = [c_void_p]
 
     @classmethod
-    def generate_psk(cls, acados_ocp, json_file='acados_ocp_nlp.json', simulink_opts=None, build=True):
-        model = acados_ocp.model
-
-        if simulink_opts is None:
-            simulink_opts = get_simulink_default_opts()
-
-        # make dims consistent
-        make_ocp_dims_consistent(acados_ocp)
-
-        # module dependent post processing
-        if acados_ocp.solver_options.integrator_type == 'GNSF':
-            set_up_imported_gnsf_model(acados_ocp)
-
-        if acados_ocp.solver_options.qp_solver == 'PARTIAL_CONDENSING_QPDUNES':
-            remove_x0_elimination(acados_ocp)
-
-        # set integrator time automatically
-        acados_ocp.solver_options.Tsim = acados_ocp.solver_options.time_steps[0]
-
-        # generate external functions
-        ocp_generate_external_functions(acados_ocp, model)
-
-        # dump to json
-        ocp_formulation_json_dump(acados_ocp, simulink_opts, json_file)
-
-        code_export_dir = acados_ocp.code_export_directory
-        # render templates
-        ocp_render_templates(acados_ocp, json_file)
-
-        if build:
-            ## Compile solver
-            cwd = os.getcwd()
-            os.chdir(code_export_dir)
-            os.system('make clean_ocp_shared_lib')
-            os.system('make ocp_shared_lib')
-            os.chdir(cwd)
-
-    @classmethod
     def generate(cls, acados_ocp, json_file='acados_ocp_nlp.json', simulink_opts=None, cmake_builder: CMakeBuilder = None):
         """
         Generates the code for an acados OCP solver, given the description in acados_ocp.
