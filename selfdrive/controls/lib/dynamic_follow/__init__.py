996869c1d618645f6c98db544879f5211a6b9121
parksunkyu81 <parksunkyu81@gmail.com>
2022-09-24 08:00:32 +0900

    Revert "Revert "...""
    This reverts commit 3c62c5fd93d670637de38e0c44a2e89dbb292ae0.

--- selfdrive/controls/lib/dynamic_follow/__init__.py
+++ selfdrive/controls/lib/dynamic_follow/__init__.py
@@ -5,7 +5,7 @@ from common.realtime import sec_since_boot, DT_MDL
 from selfdrive.controls.lib.drive_helpers import MPC_COST_LONG
 from common.op_params import opParams
 from common.numpy_fast import interp, clip, mean
-from selfdrive.config import Conversions as CV
+from common.conversions import Conversions as CV
 from cereal.messaging import SubMaster
 
 from selfdrive.controls.lib.dynamic_follow.auto_df import predict
