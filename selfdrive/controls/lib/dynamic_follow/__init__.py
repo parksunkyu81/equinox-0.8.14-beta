5bc272adec842e6a72b40bd3309bf28b773fb0b3
parksunkyu81 <parksunkyu81@gmail.com>
2022-09-23 18:34:53 +0900

    ..

--- selfdrive/controls/lib/dynamic_follow/__init__.py
+++ selfdrive/controls/lib/dynamic_follow/__init__.py
@@ -5,7 +5,7 @@ from common.realtime import sec_since_boot, DT_MDL
 from selfdrive.controls.lib.drive_helpers import MPC_COST_LONG
 from common.op_params import opParams
 from common.numpy_fast import interp, clip, mean
-from common.conversions import Conversions as CV
+from selfdrive.config import Conversions as CV
 from cereal.messaging import SubMaster
 
 from selfdrive.controls.lib.dynamic_follow.auto_df import predict
@@ -84,6 +84,7 @@ class DynamicFollow:
     self._setup_changing_variables()
 
   def _setup_collector(self):
+    self.sm_collector = SubMaster(['liveTracks', 'laneSpeed'])
     self.log_auto_df = False  # self.op_params.get('log_auto_df')
     if not isinstance(self.log_auto_df, bool):
       self.log_auto_df = False
@@ -134,6 +135,7 @@ class DynamicFollow:
       self._get_pred()  # sets self.model_profile, all other checks are inside function
 
   def _gather_data(self):
+    self.sm_collector.update(0)
     # live_tracks = [[i.dRel, i.vRel, i.aRel, i.yRel] for i in self.sm_collector['liveTracks']]
     if self.car_data.cruise_enabled:
       self.data_collector.update([self.car_data.v_ego,
@@ -141,6 +143,13 @@ class DynamicFollow:
                                   self.lead_data.a_lead,
                                   self.lead_data.v_lead,
                                   self.lead_data.x_lead,
+                                  list(self.sm_collector['laneSpeed'].leftLaneSpeeds),
+                                  list(self.sm_collector['laneSpeed'].middleLaneSpeeds),
+                                  list(self.sm_collector['laneSpeed'].rightLaneSpeeds),
+
+                                  list(self.sm_collector['laneSpeed'].leftLaneDistances),
+                                  list(self.sm_collector['laneSpeed'].middleLaneDistances),
+                                  list(self.sm_collector['laneSpeed'].rightLaneDistances),
                                   self.user_profile,
                                   sec_since_boot()])
 
