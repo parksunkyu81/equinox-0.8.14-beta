4e3623895c750330ccbcc472fdd2f0a54b568cb4
parksunkyu81 <parksunkyu81@gmail.com>
2022-09-25 14:55:35 +0900

    Tuning

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -654,7 +654,24 @@ void NvgWindow::drawBottomIcons(QPainter &p) {
   }
 
   // 5.TR Mode
+  x = radius / 2 + (bdr_s * 2) + ((radius + 50) * 4);
+  int tr_mode = controls_state.getDynamicTRMode();
 
+  p.setPen(Qt::NoPen);
+  p.setBrush(blackColor(200));
+  p.drawEllipse(x - radius / 2, y1 - radius / 2, radius, radius);
+
+  str.sprintf("%d", tr_mode);
+
+  configFont(p, "Open Sans", textSize, "Bold");
+  textColor = QColor(255, 255, 255, 200);
+
+  configFont(p, "Open Sans", 30, "Bold");
+  drawText(p, x, y1-20, "TR Value", 200);
+
+  configFont(p, "Open Sans", textSize, "Bold");
+  drawTextWithColor(p, x, y1+50, str, textColor);
+  p.setOpacity(1.0);
 
 
   // ================================================================================================================ //
@@ -757,15 +774,13 @@ void NvgWindow::drawBottomIcons(QPainter &p) {
 
   // 5.TR Value
   x = radius / 2 + (bdr_s * 2) + ((radius + 50) * 4);
-  //float tr_value = controls_state.getDynamicTRValue();
-  int tr_mode = controls_state.getDynamicTRMode();
+  float tr_value = controls_state.getDynamicTRValue();
 
   p.setPen(Qt::NoPen);
   p.setBrush(blackColor(200));
   p.drawEllipse(x - radius / 2, y1 - radius / 2, radius, radius);
 
-  //str.sprintf("%.2f", tr_value);
-  str.sprintf("%d", tr_mode);
+  str.sprintf("%.2f", tr_value);
 
   configFont(p, "Open Sans", textSize, "Bold");
   textColor = QColor(255, 255, 255, 200);
