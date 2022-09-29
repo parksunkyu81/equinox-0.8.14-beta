4edfd9626dad0ed530696c355971383ec7f59a38
parksunkyu81 <parksunkyu81@gmail.com>
2022-09-29 18:50:07 +0900

    ..

--- selfdrive/ntune.py
+++ selfdrive/ntune.py
@@ -174,6 +174,9 @@ class nTune():
     if self.checkValue("pathOffset", -1.0, 1.0, 0.0):
       updated = True
 
+    if self.checkValue("cameraOffset", -2.0, 2.0, -0.06):
+      updated = True
+
     return updated
 
   def checkValidIndi(self):
