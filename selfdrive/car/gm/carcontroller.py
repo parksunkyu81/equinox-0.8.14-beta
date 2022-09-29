32c0d7e9f73a4938b2c0aeab1730b34cce6a29f1
parksunkyu81 <parksunkyu81@gmail.com>
2022-09-30 02:02:12 +0900

    ..

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -73,7 +73,7 @@ class CarController():
         if c.active and CS.adaptive_Cruise and CS.out.vEgo > V_CRUISE_ENABLE_MIN / CV.MS_TO_KPH:
 
           PEDAL_SCALE = interp(CS.out.vEgo, [0., 18.0 * CV.KPH_TO_MS, 30 * CV.KPH_TO_MS, 60 * CV.KPH_TO_MS, 80 * CV.KPH_TO_MS],
-                                            [0.22, 0.25, 0.28, 0.29, 0.31])
+                                            [0.20, 0.22, 0.25, 0.27, 0.29])
           #pedal_offset = interp(CS.out.vEgo, [0.0, CREEP_SPEED, CREEP_SPEED*2], [-.5, 0.15, 0.2])
 
           ## =============================================== ##
