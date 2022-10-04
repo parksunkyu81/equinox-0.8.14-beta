d4b31b5d686ffc1e32f5566d00cf6d9f498e4fee
parksunkyu81 <parksunkyu81@gmail.com>
2022-10-04 16:59:17 +0900

    ..

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -17,6 +17,7 @@ from selfdrive.controls.lib.vehicle_model import VehicleModel
 
 GearShifter = car.CarState.GearShifter
 EventName = car.CarEvent.EventName
+TorqueFromLateralAccelCallbackType = Callable[[float, car.CarParams.LateralTorqueTuning, float, float, bool], float]
 
 MAX_CTRL_SPEED = (V_CRUISE_MAX + 4) * CV.KPH_TO_MS
 ACCEL_MAX = 2.0
