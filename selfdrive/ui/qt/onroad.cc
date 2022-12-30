7bf6663cd9f659fbb3c9dfbc6bfe6de2b856c1bf
parksunkyu81 <parksunkyu81@strato.co.kr>
2022-12-30 12:55:14 +0900

    tuning

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -773,9 +773,13 @@ void NvgWindow::drawBottomIcons(QPainter &p) {
 
   textColor = QColor(255, 255, 255, 200);
 
-  if(acc_bool == true) {
+  if(acc_bool == true and cur_speed > 10) {
     str = "ON";
     textColor = QColor(120, 255, 120, 200);
+  }
+  else if(acc_bool == true and cur_speed <= 10) {
+    str = "OFF";
+    textColor = QColor(254, 32, 32, 200);
   }
   else {
     str = "OFF";
