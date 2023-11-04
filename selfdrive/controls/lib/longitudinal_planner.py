c0b0fca3c567378e481339b238913055623cee76
parksunkyu81 <parksunkyu81@gmail.com>
2023-11-04 09:09:25 +0900

    tuning

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -24,20 +24,20 @@ AWARENESS_DECEL = -0.2  # car smoothly decel at .2m/s^2 when user is distracted
 #A_CRUISE_MAX_BP = [0., 15., 25., 40.]
 
 # Lookup table for turns
-_A_TOTAL_MAX_V = [1.7, 3.2]
-_A_TOTAL_MAX_BP = [20., 40.]
+#_A_TOTAL_MAX_V = [1.7, 3.2]
+#_A_TOTAL_MAX_BP = [20., 40.]
 
-_A_CRUISE_MIN_V_FOLLOWING = [-3.5, -3.0, -2.5, -2.0, -1.5]
+_A_CRUISE_MIN_V_FOLLOWING = [-3.0, -2.5, -2.0, -1.5, -1.0]
 _A_CRUISE_MIN_V = [-1.0, -1.2, -1.0, -0.7, -0.5]
-_A_CRUISE_MIN_BP = [i * CV.MPH_TO_MS for i in [0., 15., 30., 55., 85.]]
+_A_CRUISE_MIN_BP = [0., 15., 30., 55., 85.]
 
-_A_CRUISE_MAX_V = [1.1, 1.2, 1.0, 0.7, 0.65]
+_A_CRUISE_MAX_V = [1.0, 1.2, 1.0, 0.7, 0.5]
 _A_CRUISE_MAX_V_FOLLOWING = [1.7, 1.5, 1.2, 0.7, 0.65]
 _A_CRUISE_MAX_BP = _A_CRUISE_MIN_BP
 
 # Lookup table for turns - fast accel
-#_A_TOTAL_MAX_V = [3.5, 4.0, 5.0]
-#_A_TOTAL_MAX_BP = [0., 25., 55.]
+_A_TOTAL_MAX_V = [3.5, 4.0, 5.0]
+_A_TOTAL_MAX_BP = [0., 25., 55.]
 
 """def get_max_accel(v_ego):
   return interp(v_ego, A_CRUISE_MAX_BP, A_CRUISE_MAX_VALS)"""
