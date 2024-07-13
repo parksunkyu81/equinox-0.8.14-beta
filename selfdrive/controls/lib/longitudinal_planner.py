d76e9f15425a4582104bd377d8a5ab886497a992
parksunkyu81 <parksunkyu81@gmail.com>
2024-07-13 11:15:19 +0900

    Revert "tuning"
    This reverts commit d4f3a9cbf2057d55e61d9e97c440971636cf5214.

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -27,13 +27,16 @@ AWARENESS_DECEL = -0.2  # car smoothly decel at .2m/s^2 when user is distracted
 #_A_TOTAL_MAX_V = [1.7, 3.2]
 #_A_TOTAL_MAX_BP = [20., 40.]
 
+#_A_CRUISE_MIN_V_FOLLOWING = [-2.5, -2.0, -1.8, -1.5, -1.0]
+#_A_CRUISE_MIN_V = [-1.0, -1.2, -1.0, -0.7, -0.5]
+
 # 차량이 더 천천히 감속하여 멀리서 멈추게 하기 위해서는 감속 값을 더 낮춘다.
-_A_CRUISE_MIN_V_FOLLOWING = [-2.5, -2.0, -1.8, -1.5, -1.0]
-_A_CRUISE_MIN_V = [-1.0, -1.2, -1.0, -0.7, -0.5]
+_A_CRUISE_MIN_V_FOLLOWING = [-1.0, -0.8, -0.6, -0.5, -0.4]  # 감속 값을 더 낮게 설정
+_A_CRUISE_MIN_V = [-0.6, -0.5, -0.4, -0.3, -0.2]  # 감속 값을 더 낮게 설정
 _A_CRUISE_MIN_BP = [0., 15., 30., 55., 85.]
 
 _A_CRUISE_MAX_V = [1.1, 1.2, 1.0, 0.7, 0.5]
-_A_CRUISE_MAX_V_FOLLOWING = [1.6, 1.4, 1.2, 0.7, 0.65]
+_A_CRUISE_MAX_V_FOLLOWING = [1.7, 1.5, 1.2, 0.7, 0.65]
 _A_CRUISE_MAX_BP = _A_CRUISE_MIN_BP
 
 # Lookup table for turns - fast accel
