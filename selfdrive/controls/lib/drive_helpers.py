85a09eb44439fc71fd0fc7c53db2c8257b94c3fc
parksunkyu81 <sunkyu.park@lemonhc.com>
2024-08-16 15:07:35 +0900

    tuning

--- selfdrive/controls/lib/drive_helpers.py
+++ selfdrive/controls/lib/drive_helpers.py
@@ -6,7 +6,6 @@ from common.conversions import Conversions as CV
 from selfdrive.modeld.constants import T_IDXS
 from selfdrive.ntune import ntune_common_get
 
-
 # 경고: 이 값은 모델의 훈련 분포를 기반으로 결정되었으며,
 # 이 속도 이상의 모델 예측은 예측할 수 없습니다.
 
@@ -36,12 +35,10 @@ CRUISE_INTERVAL_SIGN = {
   ButtonType.decelCruise: -1,
 }
 
-
 class MPC_COST_LAT:
   PATH = 1.0
   HEADING = 1.0
   STEER_RATE = 1.0
-
 
 def apply_deadzone(error, deadzone):
   if error > deadzone:
@@ -52,26 +49,24 @@ def apply_deadzone(error, deadzone):
     error = 0.
   return error
 
-
 def rate_limit(new_value, last_value, dw_step, up_step):
   return clip(new_value, last_value + dw_step, last_value + up_step)
 
 def update_v_cruise(v_cruise_kph, buttonEvents, button_timers, enabled, metric):
-  # handle button presses. TODO: this should be in state_control, but a decelCruise press
-  # would have the effect of both enabling and changing speed is checked after the state transition
+  # 크루즈 컨트롤이 활성화되지 않은 경우 속도 변경을 적용하지 않음
   if not enabled:
     return v_cruise_kph
 
   long_press = False
   button_type = None
 
-  # should be CV.MPH_TO_KPH, but this causes rounding errors
-  v_cruise_delta = 1. if metric else 1.6
+  # 기본 속도 증가 값 설정
+  v_cruise_delta = 5  # 짧은 누름일 때 5km/h 증가
 
   for b in buttonEvents:
     if b.type.raw in button_timers and not b.pressed:
       if button_timers[b.type.raw] > CRUISE_LONG_PRESS:
-        return v_cruise_kph  # end long press
+        return v_cruise_kph  # 길게 누름 종료
       button_type = b.type.raw
       break
   else:
@@ -82,8 +77,9 @@ def update_v_cruise(v_cruise_kph, buttonEvents, button_timers, enabled, metric):
         break
 
   if button_type:
-    v_cruise_delta = v_cruise_delta * (5 if long_press else 1)
-    if long_press and v_cruise_kph % v_cruise_delta != 0:  # partial interval
+    if long_press:
+      v_cruise_delta = 10  # 길게 누를 때 10km/h 증가
+    if long_press and v_cruise_kph % v_cruise_delta != 0:  # 부분 간격일 경우
       v_cruise_kph = CRUISE_NEAREST_FUNC[button_type](v_cruise_kph / v_cruise_delta) * v_cruise_delta
     else:
       v_cruise_kph += v_cruise_delta * CRUISE_INTERVAL_SIGN[button_type]
@@ -91,15 +87,13 @@ def update_v_cruise(v_cruise_kph, buttonEvents, button_timers, enabled, metric):
 
   return v_cruise_kph
 
-
 def initialize_v_cruise(v_ego, buttonEvents, v_cruise_last):
   for b in buttonEvents:
-    # 250kph or above probably means we never had a set speed
+    # 250kph 이상일 경우 설정 속도가 없었던 것으로 간주
     if b.type == car.CarState.ButtonEvent.Type.accelCruise and v_cruise_last < 250:
       return v_cruise_last
 
   return int(round(clip(v_ego * CV.MS_TO_KPH, V_CRUISE_MIN, V_CRUISE_MAX)))
-
 
 def get_lag_adjusted_curvature(CP, v_ego, psis, curvatures, curvature_rates):
   if len(psis) != CONTROL_N:
@@ -108,17 +102,15 @@ def get_lag_adjusted_curvature(CP, v_ego, psis, curvatures, curvature_rates):
     curvature_rates = [0.0]*CONTROL_N
   v_ego = max(v_ego, 0.1)
 
-  # TODO this needs more thought, use .2s extra for now to estimate other delays
+  # TODO 이 부분은 좀 더 고민이 필요함. 현재는 .2초의 추가 지연을 사용하여 다른 지연을 추정
   delay = max(0.01, CP.steerActuatorDelay)
-  # MPC can plan to turn the wheel and turn back before t_delay. This means
-  # in high delay cases some corrections never even get commanded. So just use
-  # psi to calculate a simple linearization of desired curvature
+  # MPC가 휠을 돌리고 지연 전의 조정을 계획할 수 있음.
   current_curvature_desired = curvatures[0]
   psi = interp(delay, T_IDXS[:CONTROL_N], psis)
   average_curvature_desired = psi / (v_ego * delay)
   desired_curvature = 2 * average_curvature_desired - current_curvature_desired
 
-  # This is the "desired rate of the setpoint" not an actual desired rate
+  # 이것은 실제 목표 속도가 아닌 목표 속도의 "예상 속도"
   desired_curvature_rate = curvature_rates[0]
   max_curvature_rate = MAX_LATERAL_JERK / (v_ego**2)
   safe_desired_curvature_rate = clip(desired_curvature_rate,
